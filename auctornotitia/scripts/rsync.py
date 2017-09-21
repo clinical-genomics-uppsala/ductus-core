c433c5efaef0ff9a6ad018fa0ed3eefdb7e5266d
Patrik Smeds <patrik.smeds@gmail.com>
2017-09-21 09:45:54 +0200

    Make it possible to sync locally. Changed on variable and the description of it, it was a bit unclear what it really did.

--- auctornotitia/scripts/rsync.py
+++ auctornotitia/scripts/rsync.py
@@ -12,17 +12,19 @@ parser.add_option("-f", "--from_path", dest="from_path",
 parser.add_option("-t", "--to_path", dest="to_path",
                   help="Path to where folder/files that will be moved.")
 parser.add_option("-r", "--remote_address", dest="remote_address",
-                  help="The ip address to the remote host from where data will be pulled from or sent to.")
+                  help="The ip address to the remote host from where data will be sync from or to.")
 parser.add_option("-u", "--user", dest="user",
-                  help="he user name that will be used to move data, not required if the executing user is the one that will move the data.")
-parser.add_option("-p", "--push_or_pull", dest="push_or_pull",
-                  help="Default is to pull data, this can be setting push_or_pull to 2.", default="1")
+                  help="the user name that will be used to move data, not required if the executing user is the one that will move the data or if data will be moved locally.")
+parser.add_option("-p", "--from_is_remote", dest="from_is_remote",
+                  help="Set that \"from_address\" is the remote one, setting value 2 switch to \"to_path\" being remote.", default="1")
 parser.add_option("-i", "--identity_file", dest="identity_file",
                   help="Path to the location of the identity_file that will be used to authenticate the user.")
 parser.add_option("-c", "--checksum_validate", dest="checksum_validate", default=False, action="store_true",
                   help="Rsync command will use checksum to validate transfered files when this parameters is set to true.")
 parser.add_option("-P", "--preserve_permissions", dest="preserve_permissions", default=False, action="store_true",
                   help="Rsync command will preserve permissions and ownership.")
+parser.add_option("-l", "--local_sync", dest="local_sync", default=False, action="store_true",
+                  help="Sync will be performed locally")
 parser.add_option("-R", "--repeat", dest="repeat",
                   help="Number of times rsync command should be repeated, when it fails, default is 20 and -1 is forever.",default=20)
 
@@ -32,33 +34,34 @@ if options.from_path is None:
     parser.error("Missing argument from_path")
 if options.to_path is None:
     parser.error("Missing argument to_path")
-if options.remote_address is None:
-    parser.error("Missing argument remote_address")
-if options.user is None:
-    parser.error("Missing argument user")
-if options.identity_file is None:
-    parser.error("Missing argument identity_file")
+if options.remote_address is None and not options.local_sync:
+    parser.error("Missing argument remote_address or local sync has to be set")
+if options.user is None and not options.local_sync:
+    parser.error("Missing argument user or local sync has to be set")
+if options.identity_file is None and not options.local_sync:
+    parser.error("Missing argument identity_file or local sync has to be set")
 
 print(options.from_path,
       options.to_path,
       options.remote_address,
       options.user,
-      int(options.push_or_pull),
+      int(options.from_is_remote),
       int(options.repeat),
       options.identity_file,
-      not options.checksum_validate is None,
-      not options.preserve_permissions is None)
+      options.checksum_validate,
+      options.preserve_permissions)
 try:
     rsync=Rsync(
       options.from_path,
       options.to_path,
       options.remote_address,
       options.user,
-      int(options.push_or_pull),
+      int(options.from_is_remote),
       int(options.repeat),
       options.identity_file,
-      not options.checksum_validate is None,
-      not options.preserve_permissions is None)
+      options.checksum_validate,
+      options.preserve_permissions,
+      local_sync=options.local_sync)
     rsync.execute()
 except AddressException as err:
     logging.error("Access error, can't reach the provided address: %s", options.remote_address)
